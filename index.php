038e6c1e80700486510eea18a5dd73c8c91adc33
Matineh <matineh.mousavi8200@gmail.com>
2025-04-20 23:26:22 +0330

    add body

--- index.php
+++ index.php
@@ -11,3 +11,40 @@
   
 </body>
 </html>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">صفحه اصلی</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/webprogramming/infs.php"> اطلاعات</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/webprogramming/contact-us.php"> تماس با ما</a>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+<section>
+    <h1>بخش اول ارسال اطلاعات</h1>
+    <form action="process-form.php" method="POST">
+        <div class="form-group">
+            <label for="name">نام</label>
+            <input type="text" class="form-input" name="name" id="name" value="">
+        </div>
+        <div class="form-group">
+            <label for="last_name">نام و نام خانوادگی</label>
+            <input type="text" name="last_name" id="last_name" value="">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </form>
+</section>    
+</body>
+</html>
